4142e99348477d1d5891a144c0f45a0f3cea0d54
Hillesheimer <88099737+gfelixhilles@users.noreply.github.com>
2024-08-24 11:51:09 -0300

    Update SAM.py

--- SAM.py
+++ SAM.py
@@ -1,28 +1,35 @@
-def binario(n):
+# Making function to turn decimal into binary
+def binary(n):
+    # b is the array of each 0 and 1
     b = []
     if n == 0:
-        return n+n
+        return 0
     while n != 0:
         b.append(n%2)
         n = n // 2
+    # Inverting the order of b
     b.reverse()
     return b
-def sam(b, e, N):
+
+# Making function to do square-and-multiply
+def sam(b, p, m):
+    # r is the counter (result of the operations. Ex: r = 1; b¹ * b¹ = b² -> r = 2; b² * b² = b^4 ...)
     r = 1
-    if e == 0:
-        return e+1
+    if p == 0:
+        return p+1
     else:
-        e = binario(e)
-        e.pop(0)
-        print(e)
-        for i in e:
+        # Turning p into binary and removing the first 1
+        p = binary(p)
+        p.pop(0)
+        for i in p:
             if i == 0:
                 r = r+r
                 print("____________________________________________________")
-                print(f"{b}^{r} mod({N}) ≡ {(b**r)%N}")
+                print(f"{b}^{r} mod({m}) ≡ {(b**r)%m}")
             else:
                 r = r+r+1
                 print("____________________________________________________")
-                print(f"{b}^{r-1} mod({N}) ≡ {(b**(r-1)) % N}")
-                print(f"{b}*({b}^{r-1}) mod({N}) ≡ {(b*(b**(r-1))) % N}")
+                print(f"{b}^{r-1} mod({m}) ≡ {(b**(r-1)) % m}")
+                print(f"{b}*({b}^{r-1}) mod({m}) ≡ {(b*(b**(r-1))) % m}")
+# Making the input and printing the result
 print(sam(int(input("Digit the base: ")), int(input("Digit the power: ")), int(input("Digit the modulus: "))))
